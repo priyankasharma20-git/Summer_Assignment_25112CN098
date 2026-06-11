f37001dc00fe3d61c87b3a5c16d6c1fc51731938
PRIYANKA SHARMA <priyanka.sharma.5002@gmail.com>
2026-06-12 00:34:30 +0530

    Add files via upload

---
+++ DAY 12/Q46.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int armstrong(int n) {
+    int sum = 0, temp = n, digit;
+    while(temp > 0) {
+        digit = temp % 10;
+        sum += digit * digit * digit;
+        temp /= 10;
+    }
+    return (sum == n);
+}
+int main() {
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    if(armstrong(n))
+        printf("Armstrong Number");
+    else
+        printf("Not Armstrong Number");
+    return 0;
+}

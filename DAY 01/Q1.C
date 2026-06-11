180db4b989ce43be3be6cbffe602a92c97e4bac0
PRIYANKA SHARMA <priyanka.sharma.5002@gmail.com>
2026-06-12 00:11:54 +0530

    Add files via upload

---
+++ DAY 01/Q1.C
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int N;
+    int sum;
+    printf("Enter a number: ");
+    scanf("%d", &N);
+    sum = N * (N + 1) / 2;
+    printf("Sum of first %d natural numbers = %d", N, sum);
+    return 0;
+}

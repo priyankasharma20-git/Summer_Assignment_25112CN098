a06b29dbdb8b3bb22f91aa14502ffd903e5d25a3
PRIYANKA SHARMA <priyanka.sharma.5002@gmail.com>
2026-06-12 00:16:22 +0530

    Add files via upload

---
+++ DAY 04/Q16.C
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() {
+    int start, end, num, temp, rem, sum;
+    printf("Enter start number: ");
+    scanf("%d", &start);
+
+    printf("Enter end number: ");
+    scanf("%d", &end);
+    printf("Armstrong Numbers are:\n");
+    for(num = start; num <= end; num++) {
+        temp = num;
+        sum = 0;
+        while(temp != 0) {
+            rem = temp % 10;
+            sum = sum + rem * rem * rem;
+            temp = temp / 10;
+        }
+        if(sum == num)
+            printf("%d ", num);
+    }
+    return 0;
+}

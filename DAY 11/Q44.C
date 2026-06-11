4cc07a757763049b38e96d5a4cd1b848551e794b
PRIYANKA SHARMA <priyanka.sharma.5002@gmail.com>
2026-06-12 00:28:07 +0530

    Add files via upload

---
+++ DAY 11/Q44.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+long factorial(int n) {
+     long fact = 1;
+    for(int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+    return fact;
+}
+int main() {
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    printf("Factorial = %lld", factorial(n));
+    return 0;
+}

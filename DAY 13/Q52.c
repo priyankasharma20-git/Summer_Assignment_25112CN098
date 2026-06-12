a073d69d689bc4325a3be5626fb96141aea137cd
PRIYANKA SHARMA <priyanka.sharma.5002@gmail.com>
2026-06-12 21:18:04 +0530

    Add files via upload

---
+++ DAY 13/Q52.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+    int arr[100], n;
+    int even = 0, odd = 0;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    printf("Enter array elements:\n");
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        if(arr[i] % 2 == 0)
+            even++;
+        else
+            odd++;
+    }
+    printf("Even elements = %d\n", even);
+    printf("Odd elements = %d\n", odd);
+    return 0;
+}

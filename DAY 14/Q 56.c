f7ae8a4f1048ff496fdf49a18e27d19532623ebf
PRIYANKA SHARMA <priyanka.sharma.5002@gmail.com>
2026-06-13 23:59:30 +0530

    Add files via upload

---
+++ DAY 14/Q 56.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+    int arr[100], n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    printf("Enter array elements:\n");
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    printf("Duplicate elements are: ");
+    for(int i = 0; i < n; i++) {
+        for(int j = i + 1; j < n; j++) {
+            if(arr[i] == arr[j]) {
+                printf("%d ", arr[i]);
+                break;
+            }
+        }
+    }
+    return 0;
+}

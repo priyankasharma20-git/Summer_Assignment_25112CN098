95680ed52a392f5ab613e3103a8c52241ef9eec0
PRIYANKA SHARMA <priyanka.sharma.5002@gmail.com>
2026-06-23 11:38:51 +0530

    Add files via upload

---
+++ DAY 24/Q96.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int  flag;
+
+    printf("Enter string: ");
+    scanf("%s", str);
+
+    for(int i = 0; str[i] != '\0'; i++) {
+        flag = 0;
+        for(int j = 0; j < i; j++) {
+            if(str[i] == str[j]) {
+                flag = 1;
+                break;
+            }
+        }
+        if(flag == 0)
+            printf("%c", str[i]);
+    }
+    return 0;
+}
